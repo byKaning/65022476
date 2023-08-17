1138536d38dcb2ed5f0eee7d2b5793ecf616aa4d
byKaning <119990710+byKaning@users.noreply.github.com>
2023-08-17 11:47:06 +0700

    Create midterm.java

---
+++ midterm.java
@@ -0,0 +1,98 @@
+public class midterm {
+    class Product{
+        public String Productname;
+        public double Productprice;
+        public String getProductName(){
+            System.out.println("Product Name");
+        }
+        public String setProductName(){
+            System.out.println("chair");
+        }
+        public String getPrice(){
+            System.out.println("Product price");
+        }
+        public double setPrice(){
+            System.out.println(500.0);
+        }
+    }
+class Television{
+    public String Brand;
+    public String model;
+    public String ProductName(){
+            System.out.println("Television");
+        }
+        public double Productprice(){
+            System.out.println(20000.0);
+        }
+        public String getBrand(){
+            System.out.println("Brand");
+        }
+        public String setBrand(){
+            System.out.println("Samsung");
+        }
+        public String getModel(){
+            System.out.println("Model");
+        }
+        public String setModel(){
+            System.out.println("Neo QLED");
+        }
+ }
+ class Smartphone{
+    public String brand;
+    public String model;
+    public String ProductName(){
+            System.out.println("Iphone");
+        }
+        public double Productprice(){
+            System.out.println(40000.0);
+        }
+        public String getBrand(){
+            System.out.println("Brand");
+        }
+        public String setBrand(){
+            System.out.println("Apple");
+        }
+        public String getModel(){
+            System.out.println("Model");
+        }
+        public String setModel(){
+            System.out.println("Iphone 99");
+        }
+        public String getOperatingSystem(){
+            System.out.println("Operasion System");
+        }
+        public String setOperatingSystem(){
+            System.out.println("ios");
+        }
+ }
+ class Book {
+    public String author;
+    public int pages;
+    public String ProductName(){
+            System.out.println("OOP Programmimg");
+        }
+        public double Productprice(){
+            System.out.println(250.0);
+        }
+        public String getAuthor(){
+            System.out.println("Author");
+    
+        }
+        public String setAuthor(){
+            System.out.println("Johe Doe");
+        }
+        public String getNumberofPages(){
+            System.out.println("Number of Pages");
+        }
+        public int setNumberofPages(){
+            System.out.println(300);
+        }
+    }
+ class midterm.showDetails(){
+        System.out.println("Product");
+        System.out.println("Electronics");
+        System.out.println(" Book");
+ }
+}
+
+    
